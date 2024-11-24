6ae4ebd47d7179ea4d0fca9779bb017ebd45f198
Daniil999 <daniil999o@yandex.ru>
2024-11-24 21:27:19 +0300

    With Diagram

--- ClothesShop/obj/Debug/net8.0/ClothesShop.AssemblyInfo.cs
+++ ClothesShop/obj/Debug/net8.0/ClothesShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothesShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76cbc7525fc6b6de39715f46c7e37deb4777fbab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+801283366bd98c093f89cf34b297b771c20185f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothesShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothesShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
